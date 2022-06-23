d3c5e90417f695beb689e0d731b6ad9d20931596
Afonso Henrique <89816770+aefonso@users.noreply.github.com>
2022-06-23 15:38:43 -0300

    feat(AboutTeacher): Add Animatable and Canvas Styling

--- src/pages/AboutTeacher/index.js
+++ src/pages/AboutTeacher/index.js
@@ -1,17 +1,21 @@
 import React from "react"
 import { StyleSheet, Image, Text, View, ImageBackground } from "react-native"
 
+import * as Animatable from'react-native-animatable';
+
 export default function About() {
   return (
     
     <View style={styles.Group989}>
+      <Animatable.View animation="fadeInLeft" delay={400}>
         <Image
           style={styles.Group1}
           source={{
             uri: "https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/cy5hoftk34h-39%3A7?alt=media&token=a27d6e28-971f-4ef5-9d38-7f78f9affe73",
           }}
         />
-    <View style={styles.Iphone1313Pro29}>
+        </Animatable.View>
+        <Animatable.View animation="fadeInUp" style={styles.Iphone1313Pro29}>
         <Text style={styles.Txt542}> Bruno Gomes, o lindo !!</Text>
         <Text style={styles.Txt124}>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
@@ -21,34 +25,28 @@ export default function About() {
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
           dignissim sit volutpat sed augue quis.
         </Text>
-        <Text style={styles.Txt532}>
+        <Text style={styles.Txt124}>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
           dignissim sit volutpat sed augue quis.
         </Text>
-      </View>
+      
+      </Animatable.View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   Iphone1313Pro29: {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "flex-start",
-    alignItems: "center",
-    paddingTop: 30,
-    paddingBottom: 121,
-    paddingLeft: 100,
-    paddingRight: 68,
-    borderRadius: 50,
-    backgroundColor: "rgba(250,248,248,1)",
-    width: 390,
-    height: 844,
+    backgroundColor: '#FAF8F8',
+    flex: 1,
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    paddingStart: '5%',
+    paddingEnd: '5%',
   },
   Group989: {
     backgroundColor: '#21AEE4',
-    display: "flex",
-    flexDirection: "column",
+    flex: 1
   },
   Group1: {
     paddingLeft: 50,
@@ -56,34 +54,21 @@ const styles = StyleSheet.create({
     width: 210,
     height: 210,
     marginBottom: 20,
-    marginLeft: 100,
+    marginLeft: '22%',
+    alignItems: 'center'
   },
   Txt542: {
     fontSize: 20,
-    width: 250,
-    fontWeight: "600",
-    color: "rgba(0,0,0,1)",
-    marginBottom: 30,
-    textTransform: "uppercase"
+    marginTop: 20,
+    textTransform: "uppercase",
+    textAlign: 'center',
+    fontWeight: 'bold',
+    marginBottom: 8
   },
   Txt124: {
     fontSize: 17,
-    fontWeight: "300",
-    color: "rgba(0,0,0,1)",
-    width: 378,
     marginBottom: 15,
+    textAlign: 'justify'
   },
-  Txt124: {
-    fontSize: 17,
-    fontWeight: "300",
-    color: "rgba(0,0,0,1)",
-    width: 378,
-    marginBottom: 15,
-  },
-  Txt532: {
-    fontSize: 17,
-    fontWeight: "300",
-    color: "rgba(0,0,0,1)",
-    width: 378,
-  },
+  
 })
